e71ff033231800a33f6fb39e230105b7448e568a
MMTsallagova <margo.callagova@gmail.com>
2024-09-17 20:35:39 +0900

    initialize new entity

--- src/Controller/KanjiCardController.php
+++ src/Controller/KanjiCardController.php
@@ -2,17 +2,104 @@
 
 namespace App\Controller;
 
+use App\Entity\KanjiCard;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class KanjiCardController extends AbstractController
 {
-    #[Route('/kanji/card', name: 'app_kanji_card')]
-    public function index(): Response
+    /**
+     * @Route("/api/kanjiCard/create", name="kanjiCard_create", methods={"POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('kanji_card/index.html.twig', [
-            'controller_name' => 'KanjiCardController',
+        $params = json_decode($request->getContent(), true);
+        $kanji = new KanjiCard();
+        $kanji->setKanji($params['kanji']);
+        $kanji->setFurigana($params['furigana']);
+        $kanji->setTranslate($params['translate']);
+        $kanji->setDescription($params['description']);
+        $kanji->setKanjiListId($params['kanjiList_id']);
+        $kanji->setCreatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($kanji);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'kanji card created!', 'id' => $kanji->getId()]);
+    }
+
+    /**
+     * @Route("/api/kanjiCard/update", name="kanjiCard_update", methods={"POST"})
+     */
+    public function update(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $params = json_decode($request->getContent(), true);
+
+        $kanji = $entityManager->getRepository(KanjiCard::class)->find($params['id']);
+
+        if (!$kanji) {
+            return $this->json(['status' => "KanjiCard with id: {$params['id']} not exist!"]);
+        }
+
+        $kanji->setKanji($params['kanji'] ?? $kanji->getKanji());
+        $kanji->setFurigana($params['furigana']?? $kanji->getFurigana());
+        $kanji->setTranslate($params['translate']?? $kanji->getTranslate());
+        $kanji->setDescription($params['description']?? $kanji->getDescription());
+        $kanji->setKanjiListId($params['kanjiList_id']?? $kanji->getKanjiListId());
+        $kanji->setUpdatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($kanji);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Kanji card updated!', 'id' => $kanji->getId()]);
+    }
+
+    /**
+     * @Route("/api/kanjiCard/get/{id}", name="kanjiCard_find", methods={"GET"})
+     */
+    public function getKanjiCardById(int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $kanji = $entityManager->getRepository(KanjiCard::class)->find($id);
+
+        return $this->json([
+            'id' => $kanji->getId(),
+            'kanji' => $kanji->getKanji(),
+            'furigana' => $kanji->getFurigana(),
+            'translate' => $kanji->getTranslate(),
+            'description' => $kanji->getDescription(),
+            'kanjiList_id' => $kanji->getKanjiListId(),
+            'created_at' => $kanji->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updated_at' => $kanji->getUpdatedAt() ? $kanji->getUpdatedAt()->format('Y-m-d H:i:s') : null,
         ]);
+    }
+
+    /**
+     * @Route("/api/kanjiCard", name="kanjiCard_list", methods={"GET"})
+     */
+    public function getKanjiCardList(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $list = $entityManager->getRepository(KanjiCard::class)->findAll();
+
+        $result = [];
+
+        foreach ($list as $kanji) {
+            /** @var KanjiCard $kanji */
+            $result[] = [
+                'id' => $kanji->getId(),
+                'kanji' => $kanji->getKanji(),
+                'furigana' => $kanji->getFurigana(),
+                'translate' => $kanji->getTranslate(),
+                'description' => $kanji->getDescription(),
+                'kanjiList_id' => $kanji->getKanjiListId(),
+                'created_at' => $kanji->getCreatedAt()->format('Y-m-d H:i:s'),
+                'updated_at' => $kanji->getUpdatedAt() ? $kanji->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+            ];
+        }
+
+        return $this->json($result);
     }
 }
--- src/Controller/KanjiListController.php
+++ src/Controller/KanjiListController.php
@@ -2,17 +2,91 @@
 
 namespace App\Controller;
 
+use App\Entity\GrammarList;
+use App\Entity\KanjiList;
+use App\Entity\Vocabulary;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class KanjiListController extends AbstractController
 {
-    #[Route('/kanji/list', name: 'app_kanji_list')]
-    public function index(): Response
+    /**
+     * @Route("/api/kanji_list/create", name="kanjiList_create", methods={"POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('kanji_list/index.html.twig', [
-            'controller_name' => 'KanjiListController',
+        $params = json_decode($request->getContent(), true);
+        $kanjiList = new KanjiList();
+        $kanjiList->setTitle($params['title']);
+        $kanjiList->setCreatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($kanjiList);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Kanji list created!', 'id' => $kanjiList->getId()]);
+    }
+
+    /**
+     * @Route("/api/kanji_list/update", name="kanjiList_update", methods={"POST"})
+     */
+    public function update(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $params = json_decode($request->getContent(), true);
+
+        $kanjiList = $entityManager->getRepository(KanjiList::class)->find($params['id']);
+
+        if (!$kanjiList) {
+            return $this->json(['status' => "Kanji list with id: {$params['id']} not exist!"]);
+        }
+
+        $kanjiList->setTitle($params['title'] ?? $kanjiList->getTitle());
+        $kanjiList->setUpdatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($kanjiList);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Kanji list updated!', 'id' => $kanjiList->getId()]);
+    }
+
+    /**
+     * @Route("/api/kanji_list/get/{id}", name="kanjiList_find", methods={"GET"})
+     */
+    public function getKanjiListById(int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $kanjiList = $entityManager->getRepository(KanjiList::class)->find($id);
+
+        return $this->json([
+            'id' => $kanjiList->getId(),
+            'title' => $kanjiList->getTitle(),
+            'created_at' => $kanjiList->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updated_at' => $kanjiList->getUpdatedAt() ? $kanjiList->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+            'kanjiCard' => []
         ]);
+    }
+
+    /**
+     * @Route("/api/kanji_list", name="kanji_list", methods={"GET"})
+     */
+    public function getKanjiList_List(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $list = $entityManager->getRepository(KanjiList::class)->findAll();
+
+        $result = [];
+
+        foreach ($list as $kanjiList) {
+            /** @var KanjiList $kanjiList */
+            $result[] = [
+                'id' => $kanjiList->getId(),
+                'title' => $kanjiList->getTitle(),
+                'created_at' => $kanjiList->getCreatedAt()->format('Y-m-d H:i:s'),
+                'updated_at' => $kanjiList->getUpdatedAt() ? $kanjiList->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+            ];
+        }
+
+        return $this->json($result);
     }
 }

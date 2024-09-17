aeaa9ffdb19dc67f3fb5fc8add8a2971c64a03db
Margarita <m.callagova@gmail.com>
2024-09-17 20:35:39 +0900

    initialize new entity

--- src/Controller/GrammarController.php
+++ src/Controller/GrammarController.php
@@ -2,17 +2,102 @@
 
 namespace App\Controller;
 
+use App\Entity\Grammar;
+use App\Entity\Word;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GrammarController extends AbstractController
 {
-    #[Route('/grammar', name: 'app_grammar')]
-    public function index(): Response
+    /**
+     * @Route("/api/grammar/create", name="grammar_create", methods={"POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('grammar/index.html.twig', [
-            'controller_name' => 'GrammarController',
+        $params = json_decode($request->getContent(), true);
+        $grammar = new Grammar();
+        $grammar->setTitle($params['title']);
+        $grammar->setGrammarMeaning($params['grammar_meaning']);
+        $grammar->setDescription($params['description']);
+        $grammar->setGrammarListId($params['grammarList_id']);
+        $grammar->setCreatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($grammar);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Grammar created!', 'id' => $grammar->getId()]);
+    }
+
+    /**
+     * @Route("/api/grammar/update", name="grammar_update", methods={"POST"})
+     */
+    public function update(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $params = json_decode($request->getContent(), true);
+
+        $grammar = $entityManager->getRepository(Grammar::class)->find($params['id']);
+
+        if (!$grammar) {
+            return $this->json(['status' => "Grammar with id: {$params['id']} not exist!"]);
+        }
+
+        $grammar->setTitle($params['title'] ?? $grammar->getTitle());
+        $grammar->setGrammarMeaning($params['grammar_meaning'] ?? $grammar->getGrammarMeaning());
+        $grammar->setDescription($params['description']?? $grammar->getDescription());
+        $grammar->setGrammarListId($params['grammarList_id']?? $grammar->getGrammarListId());
+        $grammar->setUpdatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($grammar);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Grammar updated!', 'id' => $grammar->getId()]);
+    }
+
+    /**
+     * @Route("/api/grammar/get/{id}", name="grammar_find", methods={"GET"})
+     */
+    public function getGrammarById(int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $grammar = $entityManager->getRepository(Grammar::class)->find($id);
+
+        return $this->json([
+            'id' => $grammar->getId(),
+            'title' => $grammar->getTitle(),
+            'grammar_meaning' => $grammar->getGrammarMeaning(),
+            'description' => $grammar->getDescription(),
+            'grammarList_id' => $grammar->getGrammarListId(),
+            'created_at' => $grammar->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updated_at' => $grammar->getUpdatedAt() ? $grammar->getUpdatedAt()->format('Y-m-d H:i:s') : null,
         ]);
     }
+
+    /**
+     * @Route("/api/grammar", name="grammar_list", methods={"GET"})
+     */
+    public function getGrammarList(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $list = $entityManager->getRepository(Grammar::class)->findAll();
+
+        $result = [];
+
+        foreach ($list as $grammar) {
+            /** @var Grammar $grammar */
+            $result[] = [
+                'id' => $grammar->getId(),
+                'title' => $grammar->getTitle(),
+                'grammar_meaning' => $grammar->getGrammarMeaning(),
+                'description' => $grammar->getDescription(),
+                'grammarList_id' => $grammar->getGrammarListId(),
+                'created_at' => $grammar->getCreatedAt()->format('Y-m-d H:i:s'),
+                'updated_at' => $grammar->getUpdatedAt() ? $grammar->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+            ];
+        }
+
+        return $this->json($result);
+    }
+
 }
--- src/Controller/GrammarListController.php
+++ src/Controller/GrammarListController.php
@@ -2,17 +2,91 @@
 
 namespace App\Controller;
 
+use App\Entity\GrammarList;
+use App\Entity\Vocabulary;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GrammarListController extends AbstractController
 {
-    #[Route('/grammar/list', name: 'app_grammar_list')]
-    public function index(): Response
+    /**
+     * @Route("/api/grammar_list/create", name="grammarList_create", methods={"POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('grammar_list/index.html.twig', [
-            'controller_name' => 'GrammarListController',
+        $params = json_decode($request->getContent(), true);
+        $grammarList = new GrammarList();
+        $grammarList->setTitle($params['title']);
+        $grammarList->setCreatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($grammarList);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Grammar list created!', 'id' => $grammarList->getId()]);
+    }
+
+    /**
+     * @Route("/api/grammar_list/update", name="grammarList_update", methods={"POST"})
+     */
+    public function update(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $params = json_decode($request->getContent(), true);
+
+        $grammarList = $entityManager->getRepository(GrammarList::class)->find($params['id']);
+
+        if (!$grammarList) {
+            return $this->json(['status' => "GrammarList with id: {$params['id']} not exist!"]);
+        }
+
+        $grammarList->setTitle($params['title'] ?? $grammarList->getTitle());
+        $grammarList->setUpdatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($grammarList);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'GrammarList updated!', 'id' => $grammarList->getId()]);
+    }
+
+    /**
+     * @Route("/api/grammar_list/get/{id}", name="grammarList_find", methods={"GET"})
+     */
+    public function getGrammarListById(int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $grammarList = $entityManager->getRepository(GrammarList::class)->find($id);
+
+        return $this->json([
+            'id' => $grammarList->getId(),
+            'title' => $grammarList->getTitle(),
+            'created_at' => $grammarList->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updated_at' => $grammarList->getUpdatedAt() ? $grammarList->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+            'grammar' => []
         ]);
     }
+
+    /**
+     * @Route("/api/grammar_list", name="grammarList_list", methods={"GET"})
+     */
+    public function getGrammarList_List(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $list = $entityManager->getRepository(GrammarList::class)->findAll();
+
+        $result = [];
+
+        foreach ($list as $grammarList) {
+            /** @var GrammarList $grammarList */
+            $result[] = [
+                'id' => $grammarList->getId(),
+                'title' => $grammarList->getTitle(),
+                'created_at' => $grammarList->getCreatedAt()->format('Y-m-d H:i:s'),
+                'updated_at' => $grammarList->getUpdatedAt() ? $grammarList->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+            ];
+        }
+
+        return $this->json($result);
+    }
+
 }
